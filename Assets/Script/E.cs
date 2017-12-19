a016a6d10489a0ddb68127bc819c5dd344f485cc
zhuiUnity <34669462+zhuiUnity@users.noreply.github.com>
2017-12-19 21:47:10 +0800

    2017/12/19/21/48

--- Assets/Script/E.cs
+++ Assets/Script/E.cs
@@ -6,5 +6,16 @@ public class E : MonoBehaviour {
 
 	void Start()
     {
+
+        Debug.Log("1");
+        Debug.Log("2");
+        Debug.Log("3");
+        Debug.Log("4");
+        Debug.Log("5");
+        Debug.Log("6");
+        Debug.Log("7");
+        Debug.Log("8");
+        Debug.Log("9");
+        Debug.Log("10");
     }
 }
